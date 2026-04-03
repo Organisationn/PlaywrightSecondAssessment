7bdb66299b9ec67f75dfe9a29b95256a6660d73b
Your Name <your.email@example.com>
2026-04-03 13:48:58 +0530

    updated key for  base url  and db name

--- utils/dbHelper.js
+++ utils/dbHelper.js
@@ -9,7 +9,7 @@ async function getConnection(){
         port : process.env.DB_PORT || 3306,
         user : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
-        database : process.env.DB_NAME
+        database : process.env.DB_NAME_TWO
     })
 }
 
